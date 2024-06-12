8f039bc21004e970931545f8858693c2a0ac99d4
Jeken Maharjan <Maharjanjeken@gmail.com>
2024-06-12 20:49:24 +0545

    js part is remaining

---
+++ index.js
@@ -0,0 +1,105 @@
+
+// function validate(){
+//     var Name = document.getElementById("name").value;
+//     var Phone = document.getElementById("phone").value;
+//     var Email = document.getElementById("email").value;
+//     var Address = document.getElementById("address").value;
+
+    
+    
+//     if(Name == "" || Phone == "" || Email == "" || Address == "" ){
+//         alert("All field are Mandatory..!!");
+//         // document.getElementById("error1").innerHTML="*Please enter your name";
+//         // document.getElementById("error2").innerHTML="*Please enter your phone number";
+//         // document.getElementById("error3").innerHTML="*Please enter your email address";
+//         // // document.getElementById("error4").innerHTML="*Please select your gender";
+//         // document.getElementById("error5").innerHTML="*Please enter your address";
+//         // // document.getElementById("error6").innerHTML="*Please select your hobbies";
+//         // // document.getElementById("error7").innerHTML="*Please select your skills";
+//         // // return false;
+//     }
+     
+//     // else if(Name.length<3 || Name.length>50){
+//     //     document.getElementById("error1").innerHTML="*Only 3-50 letters";
+//     //     // return false;
+//     // }
+
+//     // else if(Name.length>3 || Name.length<50){
+//     //     document.getElementById("error1").innerHTML="";
+//     //     // return true;
+//     // }
+
+//     // else if(isNaN(Phone)){
+//     //     document.getElementById("error2").innerHTML="*Only Numbers are allowed...";
+//     //     return false;
+//     // }
+
+//     // else if(Phone.length=0 ||Phone.length<10 || Phone.length>10){
+//     //     document.getElementById("error2").innerHTML="*Only 10 digits";
+//     //     return false;
+//     // }
+
+//     // else if(Phone.length=10){
+//     //     document.getElementById("error2").innerHTML="";
+//     //     return true;
+//     // }
+
+//     // else if(!Email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+//     //     document.getElementById("error3").innerHTML="*Please Enter valid email..";
+//     //     return false;
+//     // }
+
+//     // else if(Email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+//     //     document.getElementById("error3").innerHTML="";
+//     //     return true;
+//     // }
+
+//     // // For radio
+
+//     // if(radio1.checked == true){
+//     //     document.getElementById("error4").innerHTML="";
+//     //     true;
+//     // }
+//     // else if(radio2.checked == true){
+//     //     document.getElementById("error4").innerHTML="";
+//     //     true;
+//     // }
+//     // else{
+//     //     document.getElementById("error4").innerHTML="*Please select Gender..";
+//     //     return false;
+//     // }
+
+//     // // Address
+//     // if(Address != ""){
+//     //     document.getElementById("error5").innerHTML="";
+//     // }
+
+//     // // For checkbox
+//     // if(document.getElementById("football").checked){
+//     //     valid = true;
+//     // }
+
+//     // else if(document.getElementById("basketball").checked){
+//     //     valid = true;
+//     // }
+
+//     // else if(document.getElementById("table-tennis").checked){
+//     //     valid = true;
+//     // }
+
+//     // else if(document.getElementById("guitar").checked){
+//     //     valid = true;
+//     // }
+
+//     // if(valid){
+//     //     alert("Your Form has been Submitted..!!")
+//     // }
+
+//     // else{
+//     //     alert("Please select Hobbies..!!");
+//     //     return false;
+//     // }
+//     else{
+//         alert("Your form has been submitted !!");
+//     }
+// }
